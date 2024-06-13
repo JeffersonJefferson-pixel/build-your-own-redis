d025698041f28c6049c3375d128a5794e49d66b3
JeffersonJefferson-pixel <erenthewizard@gmail.com>
2024-06-13 18:57:18 +0700

    add tcp client and server

---
+++ server.cpp
@@ -0,0 +1,70 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/ip.h>
+#include <errno.h>
+
+static void msg(const char *msg) {
+    fprintf(stderr, "%s\n", msg);
+}
+
+static void die(const char *msg) {
+    int err = errno;
+    fprintf(stderr, "[%d] %s\n", err, msg);
+    abort();
+}
+
+
+static void do_something(int connfd) {
+  char rbuf[64] = {};
+  ssize_t n = read(connfd, rbuf, sizeof(rbuf) - 1);
+  if (n < 0) {
+    msg("read() error");
+  }
+  printf("client says: %s\n", rbuf);
+
+  char wbuf[] = "world";
+  write(connfd, wbuf, strlen(wbuf));
+}
+
+int main() {
+  int fd = socket(AF_INET, SOCK_STREAM, 0);
+
+  int val = 1;
+  setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
+
+  // bind
+  struct sockaddr_in addr = {};
+  addr.sin_family = AF_INET;
+  addr.sin_port = ntohs(1234);
+  addr.sin_addr.s_addr = ntohl(0);
+
+  int rv = bind(fd, (const sockaddr *)&addr, sizeof(addr));
+
+  if (rv) {
+    die("bind()");
+  }
+
+  // listen
+  rv = listen(fd, SOMAXCONN);
+  if (rv) {
+    die("listen()");
+  }
+
+  while (true) {
+    // accept
+    struct sockaddr_in client_addr = {};
+    socklen_t addrlen = sizeof(client_addr);
+    int connfd = accept(fd, (struct sockaddr *)&client_addr, &addrlen);
+    if (connfd < 0) {
+      continue; // error
+    }
+
+    do_something(connfd);
+    close(connfd);
+  }
+}
