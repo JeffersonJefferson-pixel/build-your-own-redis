c934a17b70604c96bd82bd9fa5e2d8aa97a2f626
JeffersonJefferson-pixel <erenthewizard@gmail.com>
2024-07-10 00:57:46 +0700

    remove unused code

--- server.cpp
+++ server.cpp
@@ -28,73 +28,7 @@ static void die(const char *msg) {
     abort();
 }
 
-static int32_t read_full(int fd, char *buf, size_t n) {
-  while (n > 0) {
-    ssize_t rv = read(fd, buf, n);
-    if (rv <= 0) {
-      return -1; // error, or unexpected EOF
-    }
-    assert((size_t)rv <= n);
-    n -= (size_t)rv;
-    buf += rv;
-  }
-  return 0;
-}
-
-static int32_t write_all(int fd, char *buf, size_t n) {
-  while (n > 0) {
-    ssize_t rv = write(fd, buf, n);
-    if (rv <= 0) {
-      return -1; // error
-    }
-    assert((size_t)rv <= n);
-    n -= (size_t)rv;
-    buf += rv;
-  }
-  return 0;
-}
-
 const size_t k_max_msg = 4096;
-
-static int32_t one_request(int connfd) {
-  // 4 bytes header
-  char rbuf[4 + k_max_msg + 1];
-  errno = 0;
-  int32_t err = read_full(connfd, rbuf, 4);
-  if (err) {
-    if (errno == 0) {
-      msg("EOF");
-    } else {
-      msg("read() error");
-    }
-    return err;
-  }
-
-  uint32_t len = 0;
-  memcpy(&len, rbuf, 4);
-  if (len > k_max_msg) {
-    msg("too long");
-  }
-
-  // request body
-  err = read_full(connfd, &rbuf[4], len);
-  if (err) {
-    msg("read() error");
-    return err;
-  }
-
-  // do something
-  rbuf[4 + len] = '\0';
-  printf("client says: %s\n", &rbuf[4]);
-
-  // reply
-  const char reply[] = "world";
-  char wbuf[4 + sizeof(reply)];
-  len = (uint32_t)strlen(reply);
-  memcpy(wbuf, &len, 4);
-  memcpy(&wbuf[4], reply, len);
-  return write_all(connfd, wbuf, 4 + len);
-}
 
 static void fd_set_nb(int fd) {
   errno = 0;
@@ -580,23 +514,7 @@ int main() {
     if (poll_args[0].revents) {
       (void)accept_new_conn(fd2conn, fd);
     }
-
-    // accept
-    struct sockaddr_in client_addr = {};
-    socklen_t addrlen = sizeof(client_addr);
-    int connfd = accept(fd, (struct sockaddr *)&client_addr, &addrlen);
-    if (connfd < 0) {
-      continue; // error
-    }
-
-    // only server one client connection at once
-    while (true) {
-      int32_t err = one_request(connfd);
-      if (err) {
-        break;
-      }
-    }
-
-    close(connfd);
   }
+
+  return 0;
 }
